63ba17ce471dbabee3fc5431962848e50a614b1f
gwiederhecker <gsw@MacBook-Pro-4.local>
2017-08-18 18:35:34 -0300

    docs

--- pyLPD/__init__.py
+++ pyLPD/__init__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-PyLPD v0.01
+PyLPD v0.0.1.5
 
-We compile in this package many functions at our
+We compile in this package many functions used at our
 laboratory (LPD - sites.ifi.unicamp.br/LPD).
 
-These module has three major submodules:
+These goal is to build three major submodules:
 
     * MLtools : interaction with matlab, matlab-like functions
-    * VISAtools: instrument control
-    * Simtools: simulation scripts
+    * VISAtools: instrument control (not ready yet)
+    * Simtools: simulation scripts (not ready yet)
 
 
 Documentation
