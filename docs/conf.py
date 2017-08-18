5657742d3b3719cba8a15c2c899a0c0df10fd5ac
gwiederhecker <gsw@MacBook-Pro-4.local>
2017-08-18 19:28:49 -0300

    docs fix

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.inte
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
-#sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../pyLPD'))
 
 
